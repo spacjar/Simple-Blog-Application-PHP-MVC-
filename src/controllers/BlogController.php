23c7f884eb916337d144fe19124d67680d2d078b
spacjar <spacjar@gmail.com>
2024-01-14 03:13:48 +0100

    feat: init files

---
+++ src/controllers/BlogController.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__ . "/../core/Controller.php";
+    require_once __DIR__ . "/../core/Request.php";
+
+    class BlogController {
+        public function viewPost($postId) {
+            
+        }
+    }
+?>
