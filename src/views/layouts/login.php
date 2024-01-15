feb606ac1dac27a4cc6e752e2bba3023cce186d0
spacjar <spacjar@gmail.com>
2024-01-15 13:55:46 +0100

    feat: client validation

--- src/views/layouts/login.php
+++ src/views/layouts/login.php
@@ -4,20 +4,21 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>ZWA Term Project - Blog - Login</title>
+        <title>ZWA Term Project - Blog - Login</title>
 
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <!-- <script src="../assets/js/pages/auth/login-validations.js" type="module" defer></script> -->
-<body>
-    <header class="header">
-        <?php require_once __DIR__ . "/../components/_navbar.php"; ?>
-    </header>
-    <div class="container">
+        <link rel="stylesheet" href="../static/css/main.css">
+        <link rel="stylesheet" href="../static/css/pages/auth-form.css">
+    </head>
+    <body>
+        <header class="header">
+            <?php require_once __DIR__ . "/../components/_navbar.php"; ?>
+        </header>
         {{content}}
-    </div>
-</body>
+        
+        <script src="../static/js/pages/auth/login-validations.js" type="module"></script>
+    </body>
 </html>
--- src/views/register.php
+++ src/views/register.php
@@ -1,26 +1,33 @@
-<!-- <form id="sign-form" class="sign-form" method="POST" action="../controllers/"> -->
-<form id="register-form" class="auth-form" action="" method="POST">
-    <div class="sign-form__group <?php echo $model->hasError('username') ? 'is-invalid' : '' ?>">
-        <label for="username-input" class="text-regular">Username (required)</label>
-        <input type="text" id="username-input" name="username" class="input" placeholder="Username" value="<?php echo htmlspecialchars($model->username);?>">
-        <div id="username-input-message-placeholder"><?php echo $model->getFirstError("username")?></div>
+<main class="auth">
+    <div class="container">
+        <div class="auth-description">
+            <h1 class="heading-2">Register</h1>
+            <p class="text-medium">Lorem ipsum dolor sit amet adipiscing elit.</p>
+        </div>
+        <form id="register-form" class="auth-form" method="POST">
+            <div class="auth-form__group <?php echo $model->hasError('username') ? 'is-invalid' : '' ?>">
+                <label for="username-input" class="text-regular">Username (required)</label>
+                <input type="text" id="username-input" name="username" class="input" placeholder="Username" value="<?php echo htmlspecialchars($model->username);?>">
+                <div id="username-input-message-placeholder"><?php echo $model->getFirstError("username")?></div>
+            </div>
+            <div class="auth-form__group <?php echo $model->hasError('email') ? 'is-invalid' : '' ?>">
+                <label for="email-input" class="text-regular">Email (required)</label>
+                <input type="email" id="email-input" name="email" class="input" placeholder="Email" value="<?php echo htmlspecialchars($model->email);?>">
+                <div id="email-input-message-placeholder"><?php echo $model->getFirstError("email")?></div>
+            </div>
+            <div class="auth-form__group <?php echo $model->hasError('password') ? 'is-invalid' : '' ?>">
+                <label for="password-input" class="text-regular">Password (required)</label>
+                <input type="password" id="password-input" name="password" autocomplete="new-password" class="input" placeholder="Password" value="<?php echo htmlspecialchars($model->password);?>">
+                <div id="password-input-message-placeholder"><?php echo $model->getFirstError("password")?></div>
+            </div>
+            <div class="auth-form__group <?php echo $model->hasError('passwordConfirm') ? 'is-invalid' : '' ?>">
+                <label for="password-confirm-input" class="text-regular">Re-enter Password (required)</label>
+                <input type="password" id="password-confirm-input" name="passwordConfirm" autocomplete="new-password" class="input" placeholder="Re-enter Password" value="<?php echo htmlspecialchars($model->passwordConfirm);?>">
+                <div id="password-confirm-input-message-placeholder"><?php echo $model->getFirstError("passwordConfirm")?></div>
+            </div>
+            <div class="auth-form__group">
+                <button class="cta cta__primary" type="submit">Register</button>
+            </div>
+        </form>
     </div>
-    <div class="sign-form__group <?php echo $model->hasError('email') ? 'is-invalid' : '' ?>">
-        <label for="email-input" class="text-regular">Email (required)</label>
-        <input type="email" id="email-input" name="email" class="input" placeholder="Email" value="<?php echo htmlspecialchars($model->email);?>">
-        <div id="email-input-message-placeholder"><?php echo $model->getFirstError("email")?></div>
-    </div>
-    <div class="sign-form__group <?php echo $model->hasError('password') ? 'is-invalid' : '' ?>">
-        <label for="password-input" class="text-regular">Password (required)</label>
-        <input type="password" id="password-input" name="password" autocomplete="new-password" class="input" placeholder="Password" value="<?php echo htmlspecialchars($model->password);?>">
-        <div id="password-input-message-placeholder"><?php echo $model->getFirstError("password")?></div>
-    </div>
-    <div class="sign-form__group <?php echo $model->hasError('passwordConfirm') ? 'is-invalid' : '' ?>">
-        <label for="password-confirm-input" class="text-regular">Re-enter Password (required)</label>
-        <input type="password" id="password-confirm-input" name="passwordConfirm" autocomplete="new-password" class="input" placeholder="Re-enter Password" value="<?php echo htmlspecialchars($model->passwordConfirm);?>">
-        <div id="password-check-input-message-placeholder"><?php echo $model->getFirstError("passwordConfirm")?></div>
-    </div>
-    <div class="sign-form__group">
-        <button class="cta cta__primary" type="submit">Sign Up</button>
-    </div>
-</form>
+</main>
