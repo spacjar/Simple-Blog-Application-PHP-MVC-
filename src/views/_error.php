e739833ef4e4d3b8893bec486abbe9d10ceb8b4f
spacjar <spacjar@gmail.com>
2024-01-16 17:40:01 +0100

    feat: error page

--- src/views/_error.php
+++ src/views/_error.php
@@ -1,5 +1,7 @@
-<div class="container">
-   <h3>
-      <?php echo $exception->getCode() ?> - <?php echo $exception->getMessage() ?>
-   </h3>
-</div>
+<section class="error">
+   <div class="container error__content">
+      <h1>
+         <?php echo $exception->getCode() ?> - <?php echo $exception->getMessage() ?>
+      </h1>
+   </div>
+</section>
