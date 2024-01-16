a87e1da1a4fc5000b49b76fb84737d0443587be1
spacjar <spacjar@gmail.com>
2024-01-16 13:19:42 +0100

    chore: label

--- src/views/dashboard-post-new.php
+++ src/views/dashboard-post-new.php
@@ -2,12 +2,12 @@
     <h1 class="dashboard__title">New post</h1>
     <form action="" method="post" class="post__form">
         <div class="form__group">
-            <label for="title">Title</label>
+            <label for="title">Title (required)</label>
             <input type="text" name="title" id="title" class="form__input" value="<?php echo htmlspecialchars($model->title); ?>">
             <div id="email-input-message-placeholder"><?php echo $model->getFirstError("title")?></div>
         </div>
         <div class="form__group">
-            <label for="content">Body</label>
+            <label for="content">Body (required)</label>
             <textarea name="content" id="content" cols="30" rows="10" class="form__input"><?php echo htmlspecialchars($model->content); ?></textarea>
             <div id="email-input-message-placeholder"><?php echo $model->getFirstError("content")?></div>
         </div>
