41c27834841f1744f47a152dfde3c97c4e081e10
spacjar <spacjar@gmail.com>
2024-01-16 16:01:09 +0100

    feat: docs and cleanup

--- src/controllers/BlogController.php
+++ src/controllers/BlogController.php
@@ -4,25 +4,58 @@
     require_once __DIR__ . "/../models/BlogModel.php";
 
 
+    /**
+     * Class BlogController
+     * 
+     * This class is responsible for handling basic blog-related operations.
+     * It extends the base Controller class.
+     */
     class BlogController extends Controller {
 
+
+        /**
+         * Handles the list action for the blog controller.
+         *
+         * Retrieves the blog posts for the specified page and renders the 'main' template.
+         *
+         * @param Request $request The HTTP request object.
+         * @return string The HTML content of the view.
+         * @throws NotFoundException If the blog post is not found.
+         */
         public function handleList(Request $request)
         {
-            $page = $request->getQueryParam('page') ?? 1;
-            $postsPerPage = 10;
-            $posts = BlogModel::getAllBlogPosts($page, $postsPerPage);
-            return $this->render('main', ['posts' => $posts, 'page' => $page]);
-        }
-
-        public function handleDetailList(Request $request) {
             try {
-                $id = $request->getRouteParam('id');
-                $postDetail = BlogModel::getBlogPostById($id);
+                $page = intval($request->getQueryParam('page')) ?: 1;
+                $postsPerPage = 10;
+                $totalPosts = BlogModel::getAllBlogPostsCount();
+                $totalPages = ceil($totalPosts / $postsPerPage);
+                $posts = BlogModel::getAllBlogPosts($page, $postsPerPage);
+                return $this->render('main', ['posts' => $posts, 'page' => $page, 'totalPages' => $totalPages]);
             } catch (Exception $e) {
                 throw new NotFoundException();
             }
-            // $postDetail = BlogModel::getBlogPostById($id);
-            return $this->render('detail', ['postDetail' => $postDetail]);
+        }
+
+
+        /**
+         * Handles the request to display the detail of a blog post.
+         *
+         * @param Request $request The request object.
+         * @return string The HTML content of the view.
+         * @throws NotFoundException If the blog post is not found.
+         */
+        public function handleDetailList(Request $request) {
+            try {
+                $id = $request->getRouteParam('id');
+                if (filter_var($id, FILTER_VALIDATE_INT) === false) {
+                    throw new NotFoundException();
+                }
+                $id = intval($id);
+                $postDetail = BlogModel::getBlogPostById($id);
+                return $this->render('detail', ['postDetail' => $postDetail]);
+            } catch (Exception $e) {
+                throw new NotFoundException();
+            }
         }
     }
 ?>
