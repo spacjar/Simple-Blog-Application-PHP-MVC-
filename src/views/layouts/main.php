0187b485675610f76cdef1daa02d07b48a00b337
spacjar <spacjar@gmail.com>
2024-01-16 17:41:28 +0100

    fix: page title

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -8,7 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php echo $this->title ?></title>
+    <title>ZWA Term Project - Blog</title>
+    <!-- <title><?php echo $this->title ?></title> -->
 
     <link rel="stylesheet" href="../static/css/main.css">
     <link rel="stylesheet" href="../static/css/blog-post.css">
