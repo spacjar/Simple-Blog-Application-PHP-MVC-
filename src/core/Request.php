120c3c184215b0c5e976d05e3c46f45f13094d7a
spacjar <spacjar@gmail.com>
2024-01-17 10:41:33 +0100

    fix: me being stupid

--- src/core/Request.php
+++ src/core/Request.php
@@ -28,13 +28,11 @@
             $data = [];
             if ($this->isGet()) {
                 foreach ($_GET as $key => $value) {
-                    // $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
                     $data[$key] = $value;
                 }
             }
             if ($this->isPost()) {
                 foreach ($_POST as $key => $value) {
-                    // $data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
                     $data[$key] = $value;
                 }
             }
